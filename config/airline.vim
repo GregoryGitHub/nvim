6211e0aefd59d46f0fc2afe41309e197e7f74cb0
jose.grigorio <jose.grigorio@localiza.com>
2024-04-06 17:21:39 -0300

    Melhoranndo configurações

--- config/airline.vim
+++ config/airline.vim
@@ -1,3 +1,7 @@
 let g:airline#extensions#tabline#formatter = 'unique_tail'
 let g:airline_theme='bubblegum'
 let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 1
+"let g:airline#extensions#tabline#show_buffers = 1
+let g:airline#extensions#tabline#switch_buffers_and_tabs = 1
+"let g:airline#extensions#tabline#tab_nr_type = 1
--- init.lua
+++ init.lua
@@ -2,28 +2,48 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
-
-	Plug('img-paste-devs/img-paste.vim')
 	Plug('AlexvZyl/nordic.nvim', { ['branch'] = 'main' })
 	Plug('vim-airline/vim-airline')
 	Plug('vim-airline/vim-airline-themes')
 	Plug('ryanoasis/vim-devicons')
 	Plug('preservim/nerdtree')
 	Plug('mattkubej/jest.nvim')
-	Plug('voldikss/vim-floaterm')
-
+	Plug('neovim/nvim-lspconfig')
+	Plug('hrsh7th/cmp-nvim-lsp')
+	Plug('hrsh7th/cmp-buffer')
+	Plug('hrsh7th/cmp-path')
+	Plug('hrsh7th/cmp-cmdline')
+	Plug('hrsh7th/nvim-cmp')
+	Plug('onsails/lspkind.nvim')
+	Plug('OmniSharp/omnisharp-vim')	
 vim.call('plug#end')
 
 
 
 vim.cmd([[
-	set number
-	set relativenumber
-	set smarttab
-	set nowrap
-	set autochdir
-	colorscheme nordic
+set number
+set relativenumber
+set smarttab
+set nowrap
+set autochdir
+set ignorecase
+set smartcase
+colorscheme nordic
 ]])
 
-vim.cmd('source config/airline.vim')
-vim.cmd('source config/maps.vim')
+vim.cmd('source ~/.config/nvim/config/airline.vim')
+vim.cmd('source  ~/.config/nvim/config/maps.vim')
+
+
+-- Configurando o cmp ( Plugin de autocomplete )
+require'cmp'.setup {
+	sources = {
+		{ name = 'nvim_lsp' }
+	}
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require'lspconfig'.tsserver.setup {
+	capabilities = capabilities
+}
